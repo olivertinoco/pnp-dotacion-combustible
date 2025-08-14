942c89ea96e7b0b87669a2bae428f65f35a4c0e6
Analista UNITIC <analista.unitic@outlook.com>
2025-08-14 16:50:29 -0500

    maquetando tabla
    estos son estilos de tailwind aplicados a react

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,15 +5,17 @@ namespace PnpDotacionCombustible.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IWebHostEnvironment _env;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env)
     {
         _logger = logger;
+        _env = env;
     }
 
     public IActionResult Index()
     {
+        ViewBag.IsDevelopment = _env.IsDevelopment();
         return View();
     }
-    
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+// Configure CORS for Vite development server
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("ViteDevelopment", policy =>
+        {
+            policy.WithOrigins("http://localhost:5101")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+    });
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,15 +28,23 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+// Enable CORS middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("ViteDevelopment");
+}
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}"
+);
 
 app.Run();
