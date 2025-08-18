c275b8e1d723d1b9c8890f773b0bf656df7aaa96
Analista UNITIC <analista.unitic@outlook.com>
2025-08-18 15:51:47 -0500

    cambios de grilla
    se ha optimizado la grilla de carga

--- pnp-dotacion-combustible/src/App.jsx
+++ pnp-dotacion-combustible/src/App.jsx
@@ -1,17 +1,10 @@
 import BaseInput from "./components/BaseInput";
-// import { BaseTabla } from "./components/BaseTabla";
-import EjeCarga from "./components/EjeCarga";
+import { BaseTabla } from "./components/BaseTabla";
 
 export default function App() {
   return (
     <div className="p-5">
-      <div className="text-lg font-bold mb-4">
-        me integro Bien OLIVER y mili
-      </div>
-      <BaseInput />
-      <div className="my-6">SEPARA PARA... solución estamos</div>
-      {/* <BaseTabla />*/}
-      <EjeCarga />
+      <BaseTabla />
     </div>
   );
 }
